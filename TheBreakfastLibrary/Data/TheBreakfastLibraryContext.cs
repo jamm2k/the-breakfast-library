d7f9f674c03a5e7abd92d27e017fbc1056ebf51e
jose mulatinho <j4mmbr@gmail.com>
2023-03-05 16:18:10 -0300

    Deleted departments and done CRUD Scaffolding

---
+++ TheBreakfastLibrary/Data/TheBreakfastLibraryContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace TheBreakfastLibrary.Models
+{
+    public class TheBreakfastLibraryContext : DbContext
+    {
+        public TheBreakfastLibraryContext (DbContextOptions<TheBreakfastLibraryContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<TheBreakfastLibrary.Models.Department> Department { get; set; }
+    }
+}
--- TheBreakfastLibrary/Startup.cs
+++ TheBreakfastLibrary/Startup.cs
@@ -9,6 +9,8 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using TheBreakfastLibrary.Models;
 
 namespace TheBreakfastLibrary
 {
@@ -33,6 +35,9 @@ namespace TheBreakfastLibrary
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddDbContext<TheBreakfastLibraryContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("TheBreakfastLibraryContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
