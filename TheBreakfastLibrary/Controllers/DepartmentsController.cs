9ea2fda2646353576364bf3faccb28569053047b
jose mulatinho <j4mmbr@gmail.com>
2023-03-05 15:58:05 -0300

    Created first model-view-controller

---
+++ TheBreakfastLibrary/Controllers/DepartmentsController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using TheBreakfastLibrary.Models;
+
+namespace TheBreakfastLibrary.Controllers
+{
+    public class DepartmentsController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Department> list = new List<Department>();
+            list.Add(new Department { Id = 1, Name = "Romance"});
+            list.Add(new Department { Id = 2, Name = "Classics"});
+
+            return View(list);
+        }
+    }
+}
