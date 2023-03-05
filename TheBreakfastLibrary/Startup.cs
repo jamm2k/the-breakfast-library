30c574396add4e5b6f54a0d54f382371cbccbe80
jose mulatinho <j4mmbr@gmail.com>
2023-03-05 17:08:06 -0300

    MySQL adaptation and first migration

--- TheBreakfastLibrary/Startup.cs
+++ TheBreakfastLibrary/Startup.cs
@@ -37,7 +37,8 @@ namespace TheBreakfastLibrary
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
             services.AddDbContext<TheBreakfastLibraryContext>(options =>
-                    options.UseSqlServer(Configuration.GetConnectionString("TheBreakfastLibraryContext")));
+                    options.UseMySql(Configuration.GetConnectionString("TheBreakfastLibraryContext"), builder =>
+                        builder.MigrationsAssembly("TheBreakfastLibrary")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
