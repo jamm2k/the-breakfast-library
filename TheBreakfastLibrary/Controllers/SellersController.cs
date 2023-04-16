a1f9d9b9b0d64f294045cf17a37202007c933b69
jose mulatinho <j4mmbr@gmail.com>
2023-04-15 21:30:58 -0300

    SellerFormViewModel and referential integrity

--- TheBreakfastLibrary/Controllers/SellersController.cs
+++ TheBreakfastLibrary/Controllers/SellersController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using TheBreakfastLibrary.Models;
+using TheBreakfastLibrary.Models.ViewModels;
 using TheBreakfastLibrary.Services;
 
 namespace TheBreakfastLibrary.Controllers
@@ -11,10 +12,12 @@ namespace TheBreakfastLibrary.Controllers
     public class SellersController : Controller
     {
         private readonly SellerService _sellerService;
+        private readonly DepartmentService _departmentService;
 
-        public SellersController(SellerService sellerService)
+        public SellersController(SellerService sellerService, DepartmentService departmentService)
         {
             _sellerService = sellerService;
+            _departmentService = departmentService;
         }
 
         public IActionResult Index()
@@ -25,7 +28,9 @@ namespace TheBreakfastLibrary.Controllers
 
         public IActionResult Create()
         {
-            return View();
+            var departments = _departmentService.FindAll();
+            var viewModel = new SellerFormViewModel { Departments = departments };
+            return View(viewModel);
         }
 
         [HttpPost]
---
+++ TheBreakfastLibrary/Services/DepartmentService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TheBreakfastLibrary.Models;
+
+namespace TheBreakfastLibrary.Services
+{
+    public class DepartmentService
+    {
+        private readonly TheBreakfastLibraryContext _context;
+
+        public DepartmentService(TheBreakfastLibraryContext context)
+        {
+            _context = context;
+        }
+
+        public List<Department> FindAll()
+        {
+            return _context.Department.OrderBy(x => x.Name).ToList();
+        }
+    }
+}
